ce7930cd47b7ed3575b972bf8dc74be885a6ea83
WallaceQueiros <67396393+WallaceQueiros@users.noreply.github.com>
2021-09-02 23:01:23 -0300

    Atualizando

---
+++ ex067.py
@@ -0,0 +1,14 @@
+from time import sleep
+nume = cont = 0
+print('{:=^26}'.format(' TABUADA '))
+print('=' * 26)
+while True:
+    print('Digite qualquer número negativo para sair do programa.')
+    nume = int(input('Digite um número para ver sua tabuada: '))
+    if nume <= -1:
+        break
+    while cont < 10:
+        cont += 1
+        sleep(0.8)
+        print(f'{nume} x {cont:2} = {nume * cont}')
+    cont = - 1
