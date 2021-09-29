79b2e1d198fd6b4ef5920fedee1a631181805f49
WallaceQueiros <wallacequeiros13@hotmail.com>
2021-09-29 15:05:48 -0300

    Commit

---
+++ ex076.py
@@ -0,0 +1,18 @@
+print('='*39)
+print('{:^39}'. format('LISTAGEM DE PREÇOS'))
+print('='*39)
+listagem = ('Lápis',    1.75,
+            'Borracha',  2.00,
+            'Caderno', 15.90,
+            'Estojo', 25.00,
+            'Transferidor',  4.20,
+            'Compasso', 9.99,
+            'Mochila', 120.32,
+            'Caneta', 2.50,
+            'Livro', 34.90)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('='*39)
