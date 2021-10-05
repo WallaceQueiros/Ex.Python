a281e250957c451b0e4c5ebdc1f4e0d80fc2d3f9
WallaceQueiros <wallacequeiros13@hotmail.com>
2021-10-04 21:02:31 -0300

    Commit

---
+++ ex078.py
@@ -0,0 +1,13 @@
+valores = []
+for cont in range(0, 5):
+    valores.append(int(input(f'Informe o valor para a posição {cont + 1}: ')))
+print('='*39)
+print(f'Você digitou os valores {valores}')
+print(f'O maior número foi {max(valores)} nas posições ', end='')
+for c, v in enumerate(valores):
+    if v == max(valores):
+        print(f'{c + 1}... ', end='')
+print(f'\nO menor número foi {min(valores)} nas posições ', end='')
+for c, v in enumerate(valores):
+    if v == min(valores):
+        print(f'{c + 1}... ', end='')
