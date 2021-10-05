4c6ba44921d54782a5bd3234a91dd6e89693c7d5
WallaceQueiros <wallacequeiros13@hotmail.com>
2021-10-05 19:31:47 -0300

    Commit

---
+++ ex081.py
@@ -0,0 +1,14 @@
+lista = list()
+while True:
+    lista.append(int(input('Digite um número: ')))
+    opcao = str(input('Quer continuar? [Sim/Não]: ')).strip().upper()[0]
+    if opcao == 'N':
+        break
+print('-'*31)
+print(f'Essa lista possui {len(lista)} elementos.')
+(lista.sort(reverse=True))
+print(f'Os valores em ordem decrescente: {lista}')
+if 5 in lista:
+    print('O número 5 faz parte da Lista')
+else:
+    print('O número 5 não faz parte da Lista')
