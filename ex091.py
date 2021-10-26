8d195fc4dae62811ee45efcc07471181fdfa03cc
WallaceQueiros <wallacequeiros13@hotmail.com>
2021-10-26 16:49:35 -0300

    Commit

---
+++ ex091.py
@@ -0,0 +1,20 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+players = dict({'Jogador 1': randint(1, 6),
+                'Jogador 2': randint(1, 6),
+                'Jogador 3': randint(1, 6),
+                'Jogador 4': randint(1, 6)})
+ranking = list()
+print('Valores Sorteados')
+for k, v in players.items():
+    sleep(1)
+    print(f'{k} tirou: {v} ')
+print('='*26)
+print('Ranking dos Jogadores')
+ranking = sorted(players.items(), key=itemgetter(1), reverse=True)
+
+for i, v in enumerate(ranking):
+    print(f'{i +1}° Lugar {v[0]} com: {v[1]}')
+    sleep(1)
