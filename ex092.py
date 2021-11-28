01dfd0757e07d0de1e984b8782ab6d2b8da23fd6
WallaceQueiros <wallacequeiros13@hotmail.com>
2021-11-28 16:43:40 -0300

    Commit

--- ex092.py
+++ ex092.py
@@ -6,3 +6,14 @@ nasc = int(input('Ano de Nascimento: '))
 dados['idade'] = datetime.now().year - nasc
 print('Caso não tenha carteira de trabalho digite 0')
 dados['CTPS'] = int(input('Carteira de Trabalho: '))
+if dados['CTPS'] != 0:
+    dados['contratacao'] = int(input('Ano de contratação: '))
+    dados['saldo'] = int(input('Salário: '))
+    dados['aposenta'] = dados['contratacao'] - nasc + 35
+    print('-'*30)
+    print(f'Nome: {dados["nome"]}\nIdade: {dados["idade"]}\nCTPS: {dados["CTPS"]} '
+          f'\nAno Contratação: {dados["contratacao"]}\nSalário: {dados["saldo"]}'
+          f'\nAposentadoria: {dados["aposenta"]} anos ')
+if dados['CTPS'] == 0:
+    print('-' * 30)
+    print(f'Nome: {dados["nome"]}\nIdade: {dados["idade"]}\nNão possui carteira de trabalho.')
