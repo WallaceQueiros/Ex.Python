a8495e84a3a22d2e55db355325fd47b2fd8f9bc8
WallaceQueiros <wallacequeiros13@hotmail.com>
2021-12-05 15:59:35 -0300

    continuidade

--- ex094.py
+++ ex094.py
@@ -4,7 +4,11 @@ cont_pessoa = 0
 while True:
     cont_pessoa += 1
     dado_pessoa['nome'] = str(input('Informe seu nome: '))
-    dado_pessoa['sexo'] = str(input('Informe seu sexo: '))
+    while True:
+        dado_pessoa['sexo'] = str(input('Informe seu sexo: ')).upper()[0]
+        if dado_pessoa['sexo'] in 'MF':
+            break
+        print('Digite M ou F')
     dado_pessoa['idade'] = str(input('Informe sua idade: '))
     respo = str(input('Deseja cadastrar mais alguém: ')).strip().upper()[0]
     if respo == 'N':
