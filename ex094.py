1d815c3863ca6b44e4a7d04e2095b8e48a56aa54
WallaceQueiros <wallacequeiros13@hotmail.com>
2021-12-05 18:11:32 -0300

    Commit

--- ex094.py
+++ ex094.py
@@ -1,18 +1,34 @@
 dado_pessoa = dict()
 list_pessoa = list()
-cont_pessoa = 0
+soma = media = 0
 while True:
-    cont_pessoa += 1
+    dado_pessoa.clear()
     dado_pessoa['nome'] = str(input('Informe seu nome: '))
     while True:
-        dado_pessoa['sexo'] = str(input('Informe seu sexo: ')).upper()[0]
+        dado_pessoa['sexo'] = str(input('Informe seu sexo: ')).strip().upper()[0]
         if dado_pessoa['sexo'] in 'MF':
             break
         print('Digite M ou F')
-    dado_pessoa['idade'] = str(input('Informe sua idade: '))
-    respo = str(input('Deseja cadastrar mais alguém: ')).strip().upper()[0]
+    dado_pessoa['idade'] = int(input('Informe sua idade: '))
+    soma += dado_pessoa['idade']
+    list_pessoa.append(dado_pessoa.copy())
+    while True:
+        respo = str(input('Deseja cadastrar mais alguém: ')).strip().upper()[0]
+        if respo in 'SN':
+            break
+        print('Responda Sim ou Não')
     if respo == 'N':
         break
-
-print(dado_pessoa)
-print(cont_pessoa)
+print('-' * 30)
+media = soma / len(list_pessoa)
+print(f'A) A média de idade é de {media:2.1f} ')
+print(f'B) Foram cadastradas um total de {len(list_pessoa)} pessoas')
+print(f'C) As mulheres cadastradas foram: ', end='')
+for m in list_pessoa:
+    if m['sexo'] == 'F':
+        print(f'{m["nome"]}', end=', ')
+print()
+print('D) Lista de pessoas que estão acima da média: ', end='')
+for p in list_pessoa:
+    if p['idade'] >= media:
+        print(f'{p["nome"]}', end='')
