aa5088e9c148e56b3da6d088eed3212b618c366d
WallaceQueiros <wallacequeiros13@hotmail.com>
2021-12-14 15:38:52 -0300

    Commit

---
+++ ex097.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tam = len(msg)
+    print('~'*tam)
+    print(msg)
+    print('~' * tam)
+
+
+escreva(' CURSO EM VIDEO ')
+escreva(' APRENDA PYTHON')
+escreva(' GUSTAVO GUANABARA ')
