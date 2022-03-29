6729a36afb22fbff20da9c40771355b51c82f4ba
WallaceQueiros <wallacequeiros13@hotmail.com>
2022-03-29 20:11:54 -0300

    Commit

--- ex107/teste.py
+++ ex107/teste.py
@@ -1,4 +1,4 @@
-import moeda
-valor = float(input('Digite um valor: '))
+from ex107.utilidades import moeda
+from ex107.utilidades import dado
+valor = dado.leiaDinheiro('Digite um valor R$: ')
 moeda.resumo(valor, 10, 13)
-
